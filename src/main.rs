be4435b56aba72c589e48aacb3f18337160142a7
Dmitry Ponyatov <dponyatov@gmail.com>
2024-08-31 20:02:27 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
+#![allow(unused_assignments)]
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 #![allow(unused_doc_comments)]
@@ -21,19 +22,29 @@ static mut Ip: usize = 0;
 
 /// return stack
 static mut R: &'static mut [usize] = &mut [0; Rsz];
+/// return stack pointer
+static mut Rp: u16 = 0;
 
 /// data stack
 static mut D: &'static mut [isize] = &mut [0; Dsz];
+/// data stack pointer
+static mut Dp: u8 = 0;
 
 /// check memory sizes
 fn check_memory() {
-    println!("M:0x{Msz:x} R:0x{Rsz:x} D:0x{Dsz:x}");
+    eprintln!("M:0x{Msz:x} R:0x{Rsz:x} D:0x{Dsz:x}");
     const Mmin: usize = 0x10;
-    const Rmin: usize = 0x10;
-    const Dmin: usize = 0x10;
+    const Mmax: usize = u16::MAX as usize + 1;
     assert!(Msz >= Mmin);
+    assert!(Msz <= Mmax);
+    const Rmin: usize = 0x10;
+    const Rmax: usize = u16::MAX as usize + 1;
     assert!(Rsz >= Rmin);
+    assert!(Rsz <= Rmax);
+    const Dmin: usize = 0x10;
+    const Dmax: usize = u8::MAX as usize + 1;
     assert!(Dsz >= Dmin);
+    assert!(Dsz <= Dmax);
 }
 
 fn main() {
@@ -41,12 +52,13 @@ fn main() {
     unsafe {
         vm();
     }
+    tutorial();
 }
 
 unsafe fn vm() {
-    loop {
-        fetch();
-    }
+    // loop {
+    //     fetch();
+    // }
 }
 
 unsafe fn fetch() {
@@ -60,4 +72,11 @@ fn nop() {}
 /// `( -- )` stop system
 fn halt() {
     std::process::exit(0);
+}
+
+fn tutorial() {
+    // https://rutube.ru/video/b24398e527ed2154aac4cd9081b316f6/?r=wd
+    let (mut x, y) = (0, 0);
+    [x, _] = [1, 2];
+    eprintln!("x:{x}");
 }
