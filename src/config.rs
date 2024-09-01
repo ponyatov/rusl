012d51faef7cffba068a01ed4fae867b59579fdf
Dmitry Ponyatov <dponyatov@gmail.com>
2024-09-01 18:36:34 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ src/config.rs
@@ -0,0 +1,11 @@
+/// screen width, pixels
+pub const W: u32 = 240;
+/// screen height, pixels
+pub const H: u32 = 320;
+
+/// main memory size, bytes
+pub const Msz: usize = 0x10000;
+/// return stack size, cells
+pub const Rsz: usize = 0x100;
+/// data stack size, cells
+pub const Dsz: usize = 0x10;
--- src/main.rs
+++ src/main.rs
@@ -6,45 +6,45 @@
 #![allow(unused_doc_comments)]
 #![allow(unused_mut)]
 
-/// main memory size, bytes
-const Msz: usize = 0x10000;
-/// return stack size, cells
-const Rsz: usize = 0x100;
-/// data stack size, cells
-const Dsz: usize = 0x10;
+pub mod config;
 
 /// main memory
-static mut M: &'static mut [u8] = &mut [0; Msz];
+static mut M: &'static mut [u8] = &mut [0; config::Msz];
 /// compiler pointer
 static mut Cp: usize = 0;
 /// instruction pointer
 static mut Ip: usize = 0;
 
 /// return stack
-static mut R: &'static mut [usize] = &mut [0; Rsz];
+static mut R: &'static mut [usize] = &mut [0; config::Rsz];
 /// return stack pointer
 static mut Rp: u16 = 0;
 
 /// data stack
-static mut D: &'static mut [isize] = &mut [0; Dsz];
+static mut D: &'static mut [isize] = &mut [0; config::Dsz];
 /// data stack pointer
 static mut Dp: u8 = 0;
 
 /// check memory sizes
 fn check_memory() {
-    eprintln!("M:0x{Msz:x} R:0x{Rsz:x} D:0x{Dsz:x}");
+    eprintln!(
+        "M:0x{:x} R:0x{:x} D:0x{:x}",
+        config::Msz,
+        config::Rsz,
+        config::Dsz
+    );
     const Mmin: usize = 0x10;
     const Mmax: usize = u16::MAX as usize + 1;
-    assert!(Msz >= Mmin);
-    assert!(Msz <= Mmax);
+    assert!(config::Msz >= Mmin);
+    assert!(config::Msz <= Mmax);
     const Rmin: usize = 0x10;
     const Rmax: usize = u16::MAX as usize + 1;
-    assert!(Rsz >= Rmin);
-    assert!(Rsz <= Rmax);
+    assert!(config::Rsz >= Rmin);
+    assert!(config::Rsz <= Rmax);
     const Dmin: usize = 0x10;
     const Dmax: usize = u8::MAX as usize + 1;
-    assert!(Dsz >= Dmin);
-    assert!(Dsz <= Dmax);
+    assert!(config::Dsz >= Dmin);
+    assert!(config::Dsz <= Dmax);
 }
 
 fn main() {
@@ -64,6 +64,7 @@ fn main() {
         vm();
     }
     tutorial();
+    game(&argv[0]);
 }
 
 unsafe fn vm() {
@@ -90,4 +91,28 @@ fn tutorial() {
     let (mut x, y) = (0, 0);
     [x, _] = [1, 2];
     eprintln!("x:{x}");
+}
+
+extern crate sdl2;
+use std::time::Duration;
+
+use sdl2::pixels::Color;
+
+fn game(argv: &String) {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window(&argv, config::W, config::H)
+        .build()
+        .map_err(|e| e.to_string())
+        .unwrap();
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())
+        .unwrap();
+    canvas.set_draw_color(Color::RGB(0x22, 0x22, 0x22));
+    canvas.clear();
+    canvas.present();
+    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 1));
 }
