ad95399f83940d56aadd803a9e62bb82c72071b5
Dmitry Ponyatov <dponyatov@gmail.com>
2024-09-01 12:40:40 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,17 @@ fn check_memory() {
 }
 
 fn main() {
+    fn arg(argc: usize, argv: &str) {
+        eprintln!("argv[{argc}] = {argv:?}");
+    }
+    // \ arg
+    let argv: Vec<String> = std::env::args().collect();
+    let argc = argv.len();
+    arg(0, &argv[0]);
+    for (argc, argv) in argv.iter().enumerate().skip(1) {
+        arg(argc, argv);
+    }
+    // /
     check_memory();
     unsafe {
         vm();
